1628aa005b5492cbfff6555bf3efe10a4bbbd030
Kanns103 <kannsbusiness@yahoo.com>
2022-03-02 01:11:37 +0000

    Fixed touchscreen view

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+/*
+var i = 0;
+var txt = "Twitter";
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("twittertype").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+*/
