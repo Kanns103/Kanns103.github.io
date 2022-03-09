9488fab58f6e40a23e0d3c693f43286c2d36a3d4
Kanns103 <kannsbusiness@yahoo.com>
2022-03-09 21:32:07 +0000

    Added rest of code

--- scripts/main.js
+++ scripts/main.js
@@ -21,3 +21,27 @@
       mycontact.style.display = 'block';
     }
   }
+
+  function showaboutme() {
+    var myaboutme = document.getElementById('aboutme');
+    var displaySetting = myaboutme.style.display;
+
+    if (displaySetting == 'block') {
+      myaboutme.style.display = 'none'; // Hide code
+    }
+    else { // If code is hidden
+      myaboutme.style.display = 'block';
+    }
+  }
+
+  function showtweaks() {
+    var mytweak = document.getElementById('mytweaks');
+    var displaySetting = mytweak.style.display;
+
+    if (displaySetting == 'block') {
+      mytweak.style.display = 'none'; // Hide code
+    }
+    else { // If code is hidden
+      mytweak.style.display = 'block';
+    }
+  }
