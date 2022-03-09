fcaeca28b170f80bd5df5f9c8cd7046d08f1f617
Kanns103 <kannsbusiness@yahoo.com>
2022-03-09 17:45:55 +0000

    Re done website

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
-/*
-var i = 0;
-var txt = "Twitter";
-var speed = 50;
+  function showrepo() {
+    var myrepo = document.getElementById('addrepo');
+    var displaySetting = myrepo.style.display;
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("twittertype").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+    if (displaySetting == 'block') {
+      myrepo.style.display = 'none'; // Hide code
+    }
+    else { // If code is hidden
+      myrepo.style.display = 'block';
+    }
   }
-}
-*/
+
+  function showcontact() {
+    var mycontact = document.getElementById('addcontact');
+    var displaySetting = mycontact.style.display;
+
+    if (displaySetting == 'block') {
+      mycontact.style.display = 'none'; // Hide code
+    }
+    else { // If code is hidden
+      mycontact.style.display = 'block';
+    }
+  }
